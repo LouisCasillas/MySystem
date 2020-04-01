45b56b15d1fe0aba784ae8b4c7841e37254fc105
Louis Casillas <LouisCasillas@gmail.com>
2020-03-31 20:16:49 -1000

    Making the filename name more meaningful

--- scripts/start-windows.sh
+++
@@ -1,4 +0,0 @@
-# install
-qemu-system-x86_64 -boot order=d -enable-kvm -m 4096 -nic user,model=virtio -drive file=/dev/sda,format=raw -cdrom /mnt/bigbrain2/isos/Win10_1809Oct_v2_English_x64.iso -sdl
-# run normally
-#qemu-system-x86_64 -enable-kvm -m 4096 -nic user,model=virtio -drive file=/dev/sda,format=raw -sdl
