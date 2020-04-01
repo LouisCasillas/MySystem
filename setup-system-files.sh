51acdfb6797bfb651ea34ca3eab64c1313d698f8
Louis Casillas <LouisCasillas@gmail.com>
2020-03-31 20:17:24 -1000

    Adding hostname definition

--- setup-system-files.sh
+++ setup-system-files.sh
@@ -5,3 +5,5 @@ sudo chown -h oxaric:crontab /var/spool/cron/crontabs/oxaric
 sudo ln -f -s /usr/share/zoneinfo/US/Hawaii /etc/localtime
 
 echo 'US/Hawaii' | sudo tee /etc/timezone
+
+echo 'WindowsP3249P' | sudo tee /etc/hostname
