b694366e100029ade9bfd787df2a966362eadfee
Louis Casillas <LouisCasillas@gmail.com>
2020-03-31 20:50:47 -1000

    Add exercise cronjob and don't symlink the cronjob file

--- setup-system-files.sh
+++ setup-system-files.sh
@@ -1,9 +1,10 @@
-sudo ln -f -s "$(pwd)/var/spool/cron/crontabs/oxaric" /var/spool/cron/crontabs/
-
-sudo chown -h oxaric:crontab /var/spool/cron/crontabs/oxaric
-
 sudo ln -f -s /usr/share/zoneinfo/US/Hawaii /etc/localtime
 
 echo 'US/Hawaii' | sudo tee /etc/timezone
 
 echo 'WindowsP3249P' | sudo tee /etc/hostname
+
+ln -s "$(pwd)/system-sounds" ~/
+
+sudo cp "$(pwd)/var/spool/cron/crontabs/oxaric" /var/spool/cron/crontabs/
+sudo chown -h oxaric:crontab /var/spool/cron/crontabs/oxaric
