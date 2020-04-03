3b186d79aa4e9629feab2e2dca22e1009e56c745
Louis Casillas <LouisCasillas@gmail.com>
2020-04-03 11:49:01 -1000

    Changing to keep track of all individual rep counts and also the total number of reps so all history is maintained

--- scripts/exercise-popup.sh
+++ scripts/exercise-popup.sh
@@ -20,7 +20,6 @@ exercise_count=$(( ($RANDOM % $exercise_max_count) + $exercise_min_count ))
 exercise_count_total_dir="$HOME/.exercise/exercise-counts"
 exercise_count_total_file="$exercise_count_total_dir/$(echo "$exercise" | sed -e 's/ /-/g')-total.txt"
 
-
 if [ ! -d "$exercise_count_total_dir" ]; then
 	mkdir -p "$exercise_count_total_dir"
 fi
@@ -29,20 +28,19 @@ if [ $play_sound -eq 1 ]; then
 	$sound_program "$sound_file"
 fi
 
-if [ -f "$exercise_count_total_file" ]; then
-	total="$(head -n 1 "$exercise_count_total_file")"
-else
-	total=0
-fi
-
 $popup_program --no-wrap --info --title="Exercise!" --text="Time to exercise!" --display=:0.0
 
 sleep 2s
 
-$popup_program --no-wrap --timeout="$popup_timeout" --question --ok-label "Did them!" --title="Exercise!" --text="Do $exercise_count $exercise!\nTotal: $total" --display=:0.0
+$popup_program --no-wrap --timeout="$popup_timeout" --question --ok-label "Did them!" --title="Exercise!" --text="Do $exercise_count $exercise!" --display=:0.0
 
 if [ $? -eq 0 ]; then
-	total=$(( total + $exercise_count ))
-	(echo "$total"; tail -n+2 "$exercise_count_total_file") > "$exercise_count_total_file.tmp"
+	if [ -f "$exercise_count_total_file" ]; then
+		total="$(head -n 1 "$exercise_count_total_file")+"
+	else
+		total=""
+	fi
+
+	(echo "$total""$exercise_count"; tail -n+2 "$exercise_count_total_file" 2>/dev/null) > "$exercise_count_total_file.tmp"
 	mv "$exercise_count_total_file.tmp" "$exercise_count_total_file"
 fi
--- scripts/exercise-reset-total.sh
+++ scripts/exercise-reset-total.sh
@@ -10,9 +10,15 @@ exercise_count_total_dir="$HOME/.exercise/exercise-counts"
 
 find -L "$exercise_count_total_dir" -type f |
 	while read exercise_count_total_file; do
-		total="$(head -n 1 "$exercise_count_total_file")"
-		total="$(date -d "$total_days days ago" +%Y-%m-%d) to $(date +%Y-%m-%d) - $total"
+		echo "-> $exercise_count_total_file"
+		reps="$(head -n 1 "$exercise_count_total_file")"
+		total="$(echo "$reps" | bc)"
+		start_date="$(date -d "$total_days days ago" +%Y-%m-%d)"
+		end_date="$(date +%Y-%m-%d)"
+		total="$start_date to $end_date : $total : $reps"
 			
 		(echo "0"; echo "$total"; tail -n+2 "$exercise_count_total_file") > "$exercise_count_total_file.tmp"
 		mv "$exercise_count_total_file.tmp" "$exercise_count_total_file"
+
+		break
 	done
