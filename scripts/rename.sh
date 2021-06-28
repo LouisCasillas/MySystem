57d2b91b05674ebac51102265f10fcc8c13867d0
Louis Casillas <LouisCasillas@gmail.com>
2021-06-28 13:40:52 -0700

    Add filename sorting and live filename renaming

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 start_num="$1"
-tmp_file_list="$(tempfile)"
-tmp_command_file="$(tempfile)"
-tmp_command_file="cmds"
+tmp_file_list="$(mktemp)"
 
 if [[ "$start_num" == "" ]]; then 
 	echo "Must pass the starting number!"
@@ -19,31 +17,27 @@ function trap_exit()
 
 trap trap_exit SIGINT
 
-find . -type f -print0 > "$tmp_file_list"
+find . -type f -print0 | \
+	sort --zero-terminated --field-separator='/' --key=2 --numeric-sort > "$tmp_file_list"
 
-xargs -a "$tmp_file_list" -0 -n1 echo | while read old_filename; do 
+xargs -a "$tmp_file_list" -0 -n1 echo | \
+	while read old_filename; do 
 
-	extension="$(echo "$old_filename" | awk -F. '{if (NF > 2) {print "."$NF}}')"
+		extension="$(echo "$old_filename" | awk -F. '{if (NF > 2) {print "."$NF}}')"
 
-	while true; do
-		new_filename="$start_num""$extension"
+		while true; do
+			new_filename="$start_num""$extension"
 
-		if [ ! -f "$new_filename" ]; then
-			break
-		fi
+			if [[ (! -f "$new_filename") || ("$old_filename" -ef "$new_filename") ]]; then
+				break
+			fi
+
+			((start_num++));
+		done
+		
+		mv --no-clobber --verbose -- "$old_filename" "$new_filename"
 
 		((start_num++));
 	done
-	
-	echo "mv --no-clobber --verbose -- \"$old_filename\" \"$new_filename\"" >> "$tmp_command_file"
 
-	((start_num++));
-done
-exit
-tmp_file="$(tempfile)"
-tac "$tmp_command_file" > "$tmp_file"
-bash "$tmp_file"
-
-rm "$tmp_file"
 rm "$tmp_file_list"
-rm "$tmp_command_file"
