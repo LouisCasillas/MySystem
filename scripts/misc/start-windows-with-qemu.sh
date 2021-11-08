7eacd453ee5a2a48b55ec7d6e5d29415a6b60a79
Louis Casillas <louis.casillas@gmail.com>
2021-11-08 18:41:04 -0500

    Moving the qemu Windows run script

--- scripts/misc/start-windows-with-qemu.sh
+++ scripts/misc/start-windows-with-qemu.sh
@@ -1,4 +1,4 @@
-# install
+# install windows
 qemu-system-x86_64 -boot order=d -enable-kvm -m 4096 -nic user,model=virtio -drive file=/dev/nvme1n1,format=raw -cdrom /home/oxaric/Downloads/W10X64.PRO.ENU.JAN2021.iso -sdl
 # run normally
 #qemu-system-x86_64 -enable-kvm -m 4096 -nic user,model=virtio -drive file=/dev/sda,format=raw -sdl
