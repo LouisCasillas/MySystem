8fce869c1abd29e5c1e183d2e5e815a6dd819db3
Louis Casillas <louis.casillas@gmail.com>
2021-11-08 18:42:56 -0500

    Removing android scripts because they are too dangerous to be used randomly

--- scripts/android-scripts/2remove-tablet-bloatware.sh
+++
@@ -1,8 +0,0 @@
-echo "Deleting: org.simalliance.openmobileapi.service"
-pm uninstall --user 0 org.simalliance.openmobileapi.service
-echo "Deleting: com.sec.android.app.servicemodeapp"
-pm uninstall --user 0 com.sec.android.app.servicemodeapp
-echo "Deleting: com.sec.bcservice"
-pm uninstall --user 0 com.sec.bcservice
-echo "Deleting: com.sec.phone"
-pm uninstall --user 0 com.sec.phone
