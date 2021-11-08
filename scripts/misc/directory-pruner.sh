dfcf3bed44bce34958a2af641e92ae4a41237cd5
Louis Casillas <louis.casillas@gmail.com>
2021-11-08 18:10:01 -0500

    Rename and improve the directory pruner script

--- scripts/misc/directory-pruner.sh
+++ scripts/misc/directory-pruner.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-find . -depth -type d > list-of-dirs.txt
+# Script used to prune empty directories and directories with single files
+
+# Before:
+# dir1/dir2/file1
+# dir1/file2
+# dir1/dir3/
+
+# After:
+# dir1/file1
+# dir1/file2
+
+tmp_file="$(mktemp)"
+find . -depth -type d > "$tmp_file"
 
 # depth first directory finder
 while read dir; do
@@ -8,28 +20,27 @@ while read dir; do
 	#echo "---Checking dir: $dir"
 
 	# check for the number of files in this directory
-	num_of_files="$(ls "$dir" | wc -l)"
+	num_of_files="$(ls -A "$dir" | wc -l)"
 
 	#echo "Found (n) files: $num_of_files"
 
 	# if the current directory is empty then delete it
 	if [[ "$num_of_files" == "0" ]]; then
-		echo "--- Found empty directory"
+		echo "--- Found empty directory.  Deleting..."
 		rm -rf "$dir"
 	else
 		# if only one file in this directory move the file to the parent directory and delete the current directory
 		if [[ "$num_of_files" == "1" ]]; then
-			echo "--- Found single file directory"
+			echo "--- Found single file directory.  Moving file to parent directory..."
 
 			mv "$dir"/* "$dir"/..
 
+			echo "--- Directory is now empty.  Deleting..."
 			rm -rf "$dir"
-
 		fi
 	fi
 
 
-done < list-of-dirs.txt
+done < "$tmp_file"
 
-
-rm list-of-dirs.txt
+rm "$tmp_file"
