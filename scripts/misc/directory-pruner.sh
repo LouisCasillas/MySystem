183ff33fe42883eaa9988fc8c40fc886e25caf00
Louis Casillas <Louis.Casillas@gmail.com>
2021-11-10 18:37:40 -0500

    Adding debug statements and prune counter

--- scripts/misc/directory-pruner.sh
+++ scripts/misc/directory-pruner.sh
@@ -11,36 +11,49 @@
 # dir1/file1
 # dir1/file2
 
+debug=1
+
 tmp_file="$(mktemp)"
+
+[ $debug -ge 1 ] && echo -n "Creating list of directories... "
 find . -depth -type d > "$tmp_file"
+[ $debug -ge 1 ] && echo "done"
+[ $debug -ge 2 ] && echo "Total directories found: $( wc -l "$tmp_file" | cut -f1 -d' ')"
+
+num_of_pruned=0
 
 # depth first directory finder
 while read dir; do
 
-	#echo "---Checking dir: $dir"
+	[ $debug -ge 2 ] && echo "Checking dir: $dir"
 
 	# check for the number of files in this directory
 	num_of_files="$(ls -A "$dir" | wc -l)"
 
-	#echo "Found (n) files: $num_of_files"
+	[ $debug -ge 2 ] && echo "Found (n) files: $num_of_files"
 
 	# if the current directory is empty then delete it
 	if [[ "$num_of_files" == "0" ]]; then
-		echo "--- Found empty directory.  Deleting..."
+		[ $debug -ge 1 ] && echo "Found empty directory.  Deleting: $dir"
 		rm -rf "$dir"
+
+		(( num_of_pruned++ ))
 	else
 		# if only one file in this directory move the file to the parent directory and delete the current directory
 		if [[ "$num_of_files" == "1" ]]; then
-			echo "--- Found single file directory.  Moving file to parent directory..."
+			[ $debug -ge 1 ] && echo "Found single file directory.  Moving file to parent directory..."
 
 			mv "$dir"/* "$dir"/..
 
-			echo "--- Directory is now empty.  Deleting..."
+			[ $debug -ge 1 ] && echo "Found empty directory.  Deleting: $dir"
 			rm -rf "$dir"
+
+			(( num_of_pruned++ ))
 		fi
 	fi
-
 
 done < "$tmp_file"
 
 rm "$tmp_file"
+			
+[ $debug -ge 1 ] && echo "Total directories pruned: $num_of_pruned"
