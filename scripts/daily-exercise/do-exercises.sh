036500e223bbaca9c1ecf99a5a8da6c9be9d8e90
Louis Casillas <Louis.Casillas@gmail.com>
2022-06-03 07:47:35 -0400

    Making the script work with a wider range of programs

--- scripts/daily-exercise/do-exercises.sh
+++ scripts/daily-exercise/do-exercises.sh
@@ -1,26 +1,47 @@
 
 function write()
 {
-  banner -C -c '%' -w "$COLUMNS" "$1"
+  if [[ "$(which banner 2>/dev/null)" != "" ]]; then
+    banner -C -c '%' -w "$COLUMNS" "$1"
+  else
+    if [[ "$(which figlet 2>/dev/null)" != "" ]]; then
+      figlet -w "$COLUMNS" -c "$1"
+    else
+      echo "$1"
+    fi
+  fi
 }
 
 function speak()
 {
-  write "$1"
-  espeak -z -p 0.4 -s 150 -g 11 -v en-us "$1" --stdout | aplay -c 2 &>/dev/null
+  #espeak -z -p 0.4 -s 150 -g 11 -v en-us "$1" --stdout | aplay -c 2 &>/dev/null
+  espeak -z -p 0.4 -s 150 -g 11 -v en-us "$1"
 }
+
+function say()
+{
+  write "$1"
+  speak "$1"
+}
+
 
 function play()
 {
-  aplay "$1" &>/dev/null
+  if [[ "$(which aplay 2>/dev/null)" != "" ]]; then
+    aplay "$1" &>/dev/null
+  else
+    if [[ "$(which mpv 2>/dev/null)" != "" ]]; then
+      mpv "$1" &>/dev/null
+    fi
+  fi
 }
 
 function countdown()
 {
-  for ((i=$1;i > 0; i--))
+  for ((i=$1;i>0;i--))
   {
     start_time=$(date +%s.%3N)
-    speak "$i"
+    say "$i"
 
     while (true)
     do
@@ -38,7 +59,7 @@ function countdown()
 
 function warmup_speakers()
 {
-  echo "1" | aplay -c 2 -t raw &>/dev/null
+  play "$BEEP_SOUND"
   sleep 1
 }
 
@@ -52,18 +73,18 @@ TIME_FILE="exercises-for-time.txt"
 REP_FILE="exercises-for-reps.txt"
 
 warmup_speakers
-speak "Ok let's get ready to exercise!"
-speak "Get your hand squeezers, yoga blocks, and ab wheel."
+say "Ok let's get ready to exercise!"
+say "Get your hand squeezers, yoga blocks, and ab wheel."
 countdown "8"
 
 cat "$TIME_FILE" | while read exercise; do
-  speak "$exercise"
+  say "$exercise"
   sleep 1
   countdown "$TIME_PER_EXERCISE"
 done
 
 cat "$REP_FILE" | while read exercise; do
-  speak "$exercise"
+  say "$exercise"
   sleep 1
   countdown "$TIME_PER_EXERCISE"
 
