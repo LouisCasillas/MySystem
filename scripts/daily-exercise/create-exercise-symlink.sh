954772f758e8ca2bb5bee05869d2c163bad1c017
Louis Casillas <Louis.Casillas@gmail.com>
2022-06-03 10:03:22 -0400

    Adding symlink creation script

---
+++ scripts/daily-exercise/create-exercise-symlink.sh
@@ -0,0 +1,20 @@
+
+bin_dir=""
+
+for path in `echo $PATH | tr ':' '\n' | grep 'bin$'`; do
+  if [ ! -d "$path" ]; then
+    mkdir -p "$path" &>/dev/null
+  fi
+
+  if [ -w $path ]; then
+    bin_dir="$path"
+    break
+  fi
+done
+
+if [[ "$bin_dir" == "" ]]; then
+  bin_dir="~"
+fi
+
+link_file="$bin_dir/do-daily-routine"
+ln -svf "$(pwd)/do-daily-routine.sh" "$link_file"
