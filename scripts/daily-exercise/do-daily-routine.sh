a86eb8b90740f3c1c96c9f8e6548c5f92e6013b4
Louis Casillas <Louis.Casillas@gmail.com>
2022-06-03 15:51:52 -0400

    Only change directory if we are running the script that is a symlink

--- scripts/daily-exercise/do-daily-routine.sh
+++ scripts/daily-exercise/do-daily-routine.sh
@@ -1,10 +1,12 @@
-working_dir="$(dirname `readlink $0`)"
-
+working_dir="$(dirname `readlink $0` &>/dev/null)"
 (
-cd $working_dir
-bash do-box-breathing.sh
-bash do-imst-breathing.sh
-bash do-daily-exercises.sh
-bash do-fast-cardio.sh
-bash do-slow-cardio.sh
+  if [[ ! -z "$working_dir" ]]; then
+    cd $working_dir
+  fi
+
+  bash do-box-breathing.sh
+  bash do-imst-breathing.sh
+  bash do-daily-exercises.sh
+  bash do-fast-cardio.sh
+  bash do-slow-cardio.sh
 )
