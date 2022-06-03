85bd67f370d460e0a3a586a09b262e35f089883f
Louis Casillas <Louis.Casillas@gmail.com>
2022-06-03 11:56:41 -0400

    Make it work better with directly executing a symlink script

--- scripts/daily-exercise/do-daily-routine.sh
+++ scripts/daily-exercise/do-daily-routine.sh
@@ -1,5 +1,10 @@
+working_dir="$(dirname `readlink $0`)"
+
+(
+cd $working_dir
 bash do-box-breathing.sh
 bash do-imst-breathing.sh
 bash do-daily-exercises.sh
 bash do-fast-cardio.sh
 bash do-slow-cardio.sh
+)
