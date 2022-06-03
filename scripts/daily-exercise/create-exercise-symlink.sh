0f2c14a958ebe968ae3faedd7b37b3695ce0daca
Louis Casillas <Louis.Casillas@gmail.com>
2022-06-03 10:33:09 -0400

    Moving exercise script to home local bin

--- scripts/daily-exercise/create-exercise-symlink.sh
+++ scripts/daily-exercise/create-exercise-symlink.sh
@@ -1,23 +1,10 @@
 
-bin_dir=""
-
-for path in `echo $PATH | tr ':' '\n' | grep 'bin$'`; do
-  if [ ! -d "$path" ]; then
-    mkdir -p "$path" &>/dev/null
-  fi
-
-  if [ -w $path ]; then
-    bin_dir="$path"
-    break
-  fi
-done
-
-if [[ "$bin_dir" == "" ]]; then
-  bin_dir="~"
-fi
+bin_dir="~/.local/bin"
+mkdir -p "$bin_dir"
 
 link_file="$bin_dir/do-daily-routine"
 ln -svf "$(pwd)/do-daily-routine.sh" "$link_file"
 
 chmod +x *.sh
+chmod +x "$bin_dir"
 chmod +x "$link_file"
